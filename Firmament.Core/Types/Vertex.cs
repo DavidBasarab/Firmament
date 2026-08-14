edfbf04ae090172a274231f09315e8790dd40af1
David Basarab <dbasarab617@gmail.com>
2026-08-14 17:24:44 -0400

    create vertex type

---
+++ Firmament.Core/Types/Vertex.cs
@@ -0,0 +1,14 @@
+namespace Firmament.Core.Types;
+
+public class Vertex
+{
+	public float X { get; set; }
+
+	public float Y { get; set; }
+
+	public float R { get; set; }
+
+	public float G { get; set; }
+
+	public float B { get; set; }
+}
