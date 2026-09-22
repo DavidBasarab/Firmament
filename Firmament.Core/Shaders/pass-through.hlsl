d9592f0099025044fd1e75c0693c25fa44f8a8f4
David Basarab <dbasarab617@gmail.com>
2026-09-22 12:54:39 -0400

    doing lesson 2.8

--- Firmament.Core/Shaders/pass-through.hlsl
+++ Firmament.Core/Shaders/pass-through.hlsl
@@ -7,7 +7,7 @@ struct vertex_input
 struct vertex_output
 {
 	float4 position : SV_POSITION;
-	float3 color : COLOR;
+	noperspective float3 color : COLOR;
 };
 
 vertex_output vertex_main(vertex_input input)
@@ -22,5 +22,11 @@ vertex_output vertex_main(vertex_input input)
 
 float4 pixel_main(vertex_output input) : SV_TARGET
 {
-	return float4(input.color, 1.0f);
+	// float highestWeight = max(input.color.r, max(input.color.g, input.color.b));
+	//
+	// return float4(step(highestWeight, input.color), 1.0f);
+
+	// return float4(input.color, 1.0f);
+
+	return float4(floor(input.color * 16.0f) / 16.0f, 1.0f);
 }
